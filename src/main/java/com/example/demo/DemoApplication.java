7e2e44cc9dd88e1238815f128bcf5cd0bbc3aee8
Thalita Andrade <34509079+thalitaandrade@users.noreply.github.com>
2025-09-14 14:36:15 -0300

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -22,7 +22,7 @@ public class DemoApplication {
        
 	@GetMapping("/")
         public String healthCheck(){
-                return "HEALTH CHECK OK!";
+                return "HEALTH CHECK OKK!";
         }
 
 	@GetMapping("/secured")
